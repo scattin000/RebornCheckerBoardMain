77becc23a80b5bdc27ab122ea0aaa3c71c59b29a
Sarah Cattin <t-sacatt@microsoft.com>
2017-03-31 13:51:51 -0700

    not sure.

---
+++ RebornCheckerBoardMain/Migrations/201703312040310_additions.cs
@@ -0,0 +1,16 @@
+namespace RebornCheckerBoardMain.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class additions : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
